e90fc36d318b1860cda9855451fffb0879092d2d
DominikColl <DAColl@student.fullsail.edu>
2020-02-24 18:34:38 -0500

    letter grade function works as should returning letter grade based of percent grade

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,10 +1,30 @@
+const { Student } = require('../models')
 exports.landing = (req, res) => {
     res.render('landing')
 }
 exports.createProduct = async (req, res) => {
     console.log(req.body)
-    // const { dispensary, strain, consistency, type, description, rating, thcpercent } = req.body
+    let { name, percentgrade } = req.body;
+    percentgrade = parseFloat(percentgrade)
+    console.log(letterGrade(percentgrade))
+    // function for letter grade
+    await Student.create({ name, percentgrade })
+    res.render('landing')
     // await Products.create({ dispensary, strain, consistency, type, description, rating, thcpercent })
     // const products = await Products.findAll();
     // res.render('overview', { products })
+}
+letterGrade = (percentGrade) => {
+    console.log(percentGrade)
+    if (percentGrade <= 100 && percentGrade >= 90) {
+        return 'A'
+    } else if (percentGrade <= 89 && percentGrade >= 80) {
+        return 'B'
+    } else if (percentGrade <= 79 && percentGrade >= 70) {
+        return 'C'
+    } else if (percentGrade <= 69 && percentGrade >= 60) {
+        return 'D'
+    } else if (percentGrade <= 59 && percentGrade >= 0) {
+        return 'F'
+    }
 }
--- app/index.js
+++ app/index.js
@@ -1,25 +1,16 @@
 const express = require('express');
 const log = require('debug')('web:logging')
 const error = require('debug')('web:error');
+// body parser required to read body will get undefined if not used!!!
 const bodyParser = require('body-parser');
 const router = require('./routes')
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', `${__dirname}/views`);
+// very important for req.body!!!!!!!
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', router)
-// app.use(express.static('public'))
-// app.use((req, res, next) => {
-//     log('\nRUNS ONCE FOR EVERY REQUEST')
-//     setTimeout(() => { next(); }, 2000);
-// }, (req, res, next) => {
-//     log('WILL RUN WHEN NEXT IS CALLED');
-//     next();
-// })
-// app.use('/about', (req, res, next) => {
-//     log('RUNS ONLY ON the /about page');
-//     next(new Error('Not authorized'));
-// });
+
 app.use((err, req, res, next) => {
     error('ERROR FOUND:', err);
     res.sendStatus(500);
