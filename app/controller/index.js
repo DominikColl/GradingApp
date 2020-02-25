87f6f7ee04821b85cf0794f51175421d043f8b7e
DominikColl <DAColl@student.fullsail.edu>
2020-02-24 19:15:53 -0500

    CRUD complete styling and validation next

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,18 +1,49 @@
 const { Student } = require('../models')
-exports.landing = (req, res) => {
-    res.render('landing')
+exports.landing = async (req, res) => {
+    const students = await Student.findAll();
+    res.render('landing', { students })
 }
 exports.createProduct = async (req, res) => {
     console.log(req.body)
     let { name, percentgrade } = req.body;
     percentgrade = parseFloat(percentgrade)
-    console.log(letterGrade(percentgrade))
+    let lettergrade = letterGrade(percentgrade)
     // function for letter grade
-    await Student.create({ name, percentgrade })
-    res.render('landing')
-    // await Products.create({ dispensary, strain, consistency, type, description, rating, thcpercent })
+    await Student.create({ name, percentgrade, lettergrade })
+    const students = await Student.findAll();
+    res.render('landing', { students })
+
     // const products = await Products.findAll();
     // res.render('overview', { products })
+}
+exports.renderUpdateForm = async (req, res) => {
+    const { id } = req.params;
+    const pro = await Student.findByPk(id);
+    res.render('updateform', { id })
+}
+exports.findAllRenderAll = async (req, res) => {
+    const students = await Student.findAll();
+    console.log('Products below')
+    console.log(students)
+    res.render('landing', { students })
+}
+exports.updateStudent = async (req, res) => {
+    let { name, percentgrade } = req.body;
+    percentgrade = parseFloat(percentgrade)
+    let lettergrade = letterGrade(percentgrade);
+    const { id } = req.params;
+    console.log(name + percentgrade + lettergrade)
+    console.log(id)
+    // needed where param
+    await Student.update({ name, percentgrade, lettergrade }, { where: { id: id } })
+    res.redirect('/')
+    // res.json(updatedEntry)
+}
+exports.deleteStudent = async (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+    await Student.destroy({ where: { id: id } })
+    res.redirect('/')
 }
 letterGrade = (percentGrade) => {
     console.log(percentGrade)
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,4 +2,7 @@ const router = require('express').Router()
 const controller = require('../controller')
 router.get('/', controller.landing)
 router.post('/', controller.createProduct)
+router.get('/delete/:id', controller.deleteStudent)
+router.get('/update/:id', controller.renderUpdateForm)
+router.post('/update/:id', controller.updateStudent)
 module.exports = router;
